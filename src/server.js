75365b30b4fa3e05ca222aaad220eeb0ec4750f8
rysdid17 <ryu23800@gmail.com>
2022-08-30 23:17:19 +0900

    feat: 1st step to apply socket.io

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import http from "http";
-import WebSocket from "ws";
+import SocketIO from "socket.io"
 import express from 'express';
 
 const app = express();
@@ -10,28 +10,12 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
-const handleListen = () => console.log("Listening on http://localhost:3000");
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
 
-const sockets = [];
-
-wss.on("connection", (socket) => {
-  sockets.push(socket);
-  socket["nickname"] = "Anonymous";
-  console.log("Connected to Browser");
-  socket.on("close", () => console.log("Disconnected from Browser"));
-  socket.on("message", (msg) => {
-    const message = JSON.parse(msg);
-    switch(message.type) {
-      case "new_message":
-        sockets.forEach(aSocket => aSocket.send(`${socket.nickname}: ${message.payload}`));
-        break;
-      case "nickname":
-        socket["nickname"] = message.payload;
-        break;
-    }
-  })
+wsServer.on("connection", (socket) => {
+  console.log(socket);
 })
 
-server.listen(3000, handleListen);
+const handleListen = () => console.log("listening on http://localhost:3000");
+httpServer.listen(3000, handleListen);
